6f669258966845a9020c9776528521adc376ede3
ThomassMathisen <thomasmathisen@hotmail.com>
2024-03-14 17:31:40 +0100

    Homepage design and search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./layout.scss";
 import Navbar from "./components/navbar/Navbar.jsx";
+import HomePage from "./routes/homePage/HomePage.jsx";
 
 function App() {
   return (
     <div className="layout">
       <Navbar />
+      <HomePage />
     </div>
   );
 }
---
+++ src/routes/homePage/HomePage.jsx
@@ -0,0 +1,40 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Home</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            ullam eum natus quaerat, suscipit quas vero. Nam velit eum pariatur
+            quaerat. Qui earum distinctio excepturi repudiandae reprehenderit
+            maxime accusamus obcaecati?
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Properties Available</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
